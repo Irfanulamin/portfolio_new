333d1949b4f62452684221e2d1ff97b1b89d73d5
Irfanulamin <irfanulamin10@gmail.com>
2024-05-07 09:53:00 +0600

    vercel.json added

---
+++ src/utils/data/blogData.ts
@@ -0,0 +1,56 @@
+export const blogPosts = [
+  {
+    blog_name: "Frontend Frameworks Comparison",
+    blog_image: "https://i.ibb.co/Z83hvpB/Top-Front-End-Frameworks.jpg",
+    blog_description:
+      "React, Vue, and Angular are the most popular Javascript Frameworks. where React is one the most popular one and more used framework. React is a type of JavaScript library used for building websites and apps. It lets developers create reusable pieces of user interface called components.It updates parts of a webpage efficiently when things change, making apps faster.",
+    time_published: "2024-04-15 10:30 AM",
+  },
+  {
+    blog_name: "TypeScript Fundamentals",
+    blog_image:
+      "https://i.ibb.co/q17xyBN/Java-Script-vs-Type-Script-Key-Comparison-1.jpg",
+    blog_description:
+      "If you're new to programming, TypeScript is more suitable than JavaScript. When you write code for your blog or website, you often use JavaScript to make things happen, like showing text or handling clicks. TypeScript is similar to JavaScript, but it adds extra rules to help you avoid mistakes.",
+    time_published: "2024-04-14 3:45 PM",
+  },
+  {
+    blog_name: "Next.js Deep Dive",
+    blog_image: "https://i.ibb.co/cLzwSCt/next-js-logo.png",
+    blog_description:
+      "Next.js is a popular tool for building websites.Next.js makes it simple to begin your project without starting from scratch.It helps you manage different pages for your blog, like posts, about, or contact.Next.js helps your website appear better in search engines like Google.Next.js makes your blog feel smooth and quick when visitors open it.",
+    time_published: "2024-04-12 9:00 AM",
+  },
+  {
+    blog_name: "Mastering Redux State Management",
+    blog_image:
+      "https://i.ibb.co/brJLJFB/98c389e869de0e19208f256d51760e66-1.webp",
+    blog_description:
+      "Understanding Redux Principles: Start by the fundamental concepts of Redux, including actions, reducers, store, and state management. Learn how actions are dispatched to update the state through pure functions (reducers), and how the Redux store holds the application state.Learn how to dispatch asynchronous actions for data fetching or API calls.",
+    time_published: "2024-04-10 11:20 AM",
+  },
+  {
+    blog_name: "Responsive Design Best Practices",
+    blog_image:
+      "https://i.ibb.co/bPb6RNt/0-responsive-web-design-hero-image.jpg",
+    blog_description:
+      "Implement CSS media queries to adjust styles based on screen size. Define breakpoints where your layout or content should change to optimize for different devices like smartphones, tablets, and desktops.I use relative units like rem or em instead of fixed pixels for widths, heights, and margins.Embrace CSS Grid or Flexbox for creating flexible and adaptive layouts that automatically adjust based on screen size.",
+    time_published: "2024-04-09 5:15 PM",
+  },
+  {
+    blog_name: "Optimizing Performance in Web Apps",
+    blog_image:
+      "https://i.ibb.co/QK86MfG/what-is-a-web-application-640x427.jpg",
+    blog_description:
+      "Use tools to minify and compress CSS, JavaScript, and images to reduce load times, (tips use lazyloader on images). Implement browser caching for static assets and utilize service workers for offline capabilities.Improve backend performance by optimizing database queries, leveraging caching mechanisms, and adopting efficient server-side technologies.",
+    time_published: "2024-04-08 2:00 PM",
+  },
+  {
+    blog_name: "UI/UX Design Principles",
+    blog_image:
+      "https://i.ibb.co/BLqLYKN/what-are-the-opportunities-for-ui-ux-designers-165530x.jpg",
+    blog_description:
+      "Keep your layout clean and organized with easy-to-navigate sections like header, main content area, and sidebar for categories or recent posts.Use legible fonts, appropriate font sizes, and sufficient line spacing to enhance readability.Emphasize important elements like post titles and headings using larger fonts or bold styles, and use contrasting colors for call-to-action buttons or links.",
+    time_published: "2024-04-05 4:40 PM",
+  },
+];
---
+++ src/utils/data/myProjects.ts
@@ -0,0 +1,66 @@
+const project_data = [
+  {
+    project_name: "Z-Store",
+    project_image:
+      "https://i.ibb.co/x31Bnzp/screencapture-zstore-client-vercel-app-2024-05-06-21-31-53.png",
+    project_description:
+      "Welcome to Z Store, your ultimate destination for men's fashion essentials At Z Store, we're dedicated to curating the finest collection of clothing that reflects style, quality, and comfort. Whether you're looking for timeless classics or the latest trends. ",
+    features: [
+      "Ecommerce Typescript and NextJS Website",
+      "Admin Dashboard (Read Only)",
+      "Filter Search Query",
+      "Static Side Rendering and SSG",
+    ],
+    github_link: "https://github.com/Irfanulamin/ZSTORE-client",
+    deploy_link: "https://zstore-client.vercel.app/",
+  },
+  {
+    project_name: "HealthUnity",
+    project_image:
+      "https://i.ibb.co/N6GLh4b/screencapture-healthunity-client-vercel-app-2024-05-06-21-31-34.png",
+    project_description:
+      "Welcome to HealthUnity, your trusted platform for health-related information and resources. Explore our website to discover a wide range of features designed to enhance your well-being, including medical supplies, health tips, and community support.",
+    features: [
+      "Typescript ",
+      "Admin Panel/Dashboard ",
+      "Curd Operation",
+      "Login Authentication",
+      "Dark/Light Theme",
+      "Redux and RTK Query",
+    ],
+    github_link: "https://github.com/Irfanulamin/healthunity-client",
+    deploy_link: "https://healthunity-client.vercel.app/",
+  },
+  {
+    project_name: "Vocal Studio",
+    project_image: "https://i.ibb.co/nnDLfsH/vocal-studio.png",
+    project_description:
+      "Vocal Studio is a MERN stack website that serves as a platform for learning musical instruments. The website features three different dashboards: instructor, student, and admin.",
+    features: [
+      "MERN Website",
+      "Admin, Instructor, Student ( 3 Dashboards )",
+      "Friendly UI/UX",
+      "JWT Token",
+      "Firebase Authentication",
+    ],
+    github_link: "https://github.com/Irfanulamin/Vocal-Client-Side",
+    deploy_link: "https://vocal-studio-auth.web.app/",
+  },
+
+  {
+    project_name: "Cloud Palace",
+    project_image: "https://i.ibb.co/BcQW686/cloud-palace.png",
+    project_description:
+      "An e-commerce website offering a wide range of high-quality soft toys for sale providing a delightful shopping experience for toy enthusiasts of all ages.",
+    features: [
+      "Admin Panel",
+      "Can also add Toys and also can be view toy details",
+      "Friendly UI/UX",
+      "Firebase Authentication",
+    ],
+    github_link: "https://github.com/Irfanulamin/cloud-palace",
+    deploy_link: "https://cloud-palace-auth.web.app/",
+  },
+];
+
+export default project_data;
