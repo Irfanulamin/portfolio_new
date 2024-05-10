c009de8097521508245755999e287778df8091e0
Irfanulamin <irfanulamin10@gmail.com>
2024-05-11 04:54:02 +0600

    dashboard menubar responsive has been added

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
+import dashboardImage from "@/asset/dashboard.jpg";
 
 const DashboardHomepage = () => {
-  return <div>DashboardHomepage</div>;
+  return (
+    <div className="min-h-[90vh] max-h-full w-full mx-auto">
+      <div className="py-5">
+        <h2 className="text-center text-7xl font-semibold ">
+          Welcome To Admin Panel, Soldier!
+        </h2>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <Image
+          alt="dashboard-image"
+          src={dashboardImage}
+          className="h-[50rem] w-[50rem] object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardHomepage;
--- src/components/shared/DashboardBar.tsx
+++ src/components/shared/DashboardBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Menubar,
@@ -7,8 +8,24 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { FaTools } from "react-icons/fa";
-import { IoCloudUploadSharp } from "react-icons/io5";
+import { IoCloudUploadSharp, IoMenu } from "react-icons/io5";
 import { MdSettings, MdWebStories } from "react-icons/md";
 import { PiBagFill } from "react-icons/pi";
 import { RiFileTextFill } from "react-icons/ri";
@@ -16,7 +33,7 @@ import Link from "next/link";
 
 const DashboardBar = () => {
   return (
-    <div className="flex justify-between items-center py-7">
+    <div className="flex justify-between flex-wrap items-center py-7">
       <div>
         <Link href="/dashboard">
           <h1 className="text-xl  lg:text-3xl font-extrabold  text-white">
@@ -24,7 +41,7 @@ const DashboardBar = () => {
           </h1>
         </Link>
       </div>
-      <div>
+      <div className="hidden md:block lg:block">
         <Menubar>
           <MenubarMenu>
             <MenubarTrigger>
@@ -125,6 +142,106 @@ const DashboardBar = () => {
             </MenubarContent>
           </MenubarMenu>
         </Menubar>
+      </div>
+      <div className="block md:hidden lg:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <IoMenu className="w-6 h-6 text-white" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>
+              <div className="flex  items-center gap-2 bg-black p-1">
+                <div>
+                  <p className="text-base font-medium text-white">Skills</p>
+                </div>
+                <div>
+                  <FaTools className="text-white" />
+                </div>
+              </div>
+            </DropdownMenuLabel>
+
+            <DropdownMenuGroup>
+              <Link href="/dashboard/skills/upload-skills">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Upload Skills
+                </DropdownMenuItem>
+              </Link>
+              <Link href="/dashboard/skills/modify-skills">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Modify/Delete Skills
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuGroup>
+            <DropdownMenuLabel>
+              <div className="flex  items-center gap-2 bg-black p-1">
+                <div>
+                  <p className="text-base font-medium text-white">Projects</p>
+                </div>
+                <div>
+                  <MdWebStories className="text-white" />
+                </div>
+              </div>
+            </DropdownMenuLabel>
+
+            <DropdownMenuGroup>
+              <Link href="/dashboard/projects/upload-projects">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Upload Projects
+                </DropdownMenuItem>
+              </Link>
+              <Link href="/dashboard/projects/modify-projects">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Modify/Delete Projects
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuGroup>
+            <DropdownMenuLabel>
+              <div className="flex  items-center gap-2 bg-black p-1">
+                <div>
+                  <p className="text-base font-medium text-white">Blogs</p>
+                </div>
+                <div>
+                  <RiFileTextFill className="text-white" />
+                </div>
+              </div>
+            </DropdownMenuLabel>
+
+            <DropdownMenuGroup>
+              <Link href="/dashboard/blogs/upload-blogs">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Upload Blogs
+                </DropdownMenuItem>
+              </Link>
+              <Link href="/dashboard/blogs/modify-blogs">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Modify/Delete Blogs
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuGroup>
+            <DropdownMenuLabel>
+              <div className="flex  items-center gap-2 bg-black p-1 ">
+                <div>
+                  <p className="text-base font-medium text-white">Experience</p>
+                </div>
+                <div>
+                  <PiBagFill className="text-white" />
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuGroup>
+              <Link href="/dashboard/experience/upload-exp">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Upload Experience
+                </DropdownMenuItem>
+              </Link>
+              <Link href="/dashboard/experience/modify-exp">
+                <DropdownMenuItem className="text-sm font-medium">
+                  Modify/Delete Experience
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
